b4bdef11d50f1202444c7f7ddd73ee7322956f86
Renato Kuurstra <kuurstra.renato@gmail.com>
2019-01-26 11:45:44 +0000

    added Script for dynamic grid, random placer items

---
+++ Assets/Scripts/CheckBoardMaker.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CheckBoardMaker : MonoBehaviour
+{
+    public float Distance;
+    public float Separation;
+    public int lateralSize = 3;
+    public List<BoxCollider> BoxColliders = new List<BoxCollider>();
+
+    // Start is called before the first frame update
+#if UNITY_EDITOR
+    void Update()
+    {
+        DestroyEverything();
+        CreateCheckBoard();
+    }
+#endif
+
+    void CreateCheckBoard()
+    {
+        Vector3 colliderSize = new Vector3(Distance - Separation, 15f, Distance - Separation);
+        Vector3 offSetGridToCenter = new Vector3(((Distance * lateralSize) / 2f) - Distance / 2f, 0f, ((Distance * lateralSize) / 2f) - Distance / 2f);
+        //Fuck the for loops, It's a JAMMMMMM!
+        BoxColliders = new List<BoxCollider>();
+        int totalBoxes = lateralSize * lateralSize;
+
+        for (int i = 0; i < totalBoxes; i++)
+        {
+            AddBoxCollider(colliderSize, offSetGridToCenter, i);
+        }
+    }
+
+    void AddBoxCollider(Vector3 boxSize, Vector3 offSetGridToCenter, int currentIndex)
+    {
+        GameObject gameObject = new GameObject();
+        BoxColliders.Add(gameObject.AddComponent<BoxCollider>());
+        BoxColliders[currentIndex].name = "CharTile_" + currentIndex / lateralSize + "_" + currentIndex % lateralSize;
+        // Ignore Raycast layer
+        BoxColliders[currentIndex].gameObject.layer = 2;
+        BoxColliders[currentIndex].transform.parent = this.transform;
+        BoxColliders[currentIndex].size = boxSize;
+        BoxColliders[currentIndex].transform.localPosition = new Vector3(((currentIndex / lateralSize) * Distance ) - offSetGridToCenter.x, 0f, ( (currentIndex % lateralSize) * Distance) - offSetGridToCenter.z);
+    }
+
+    void DestroyEverything()
+    {
+        Transform[] allgameObjects = this.transform.GetComponentsInChildren<Transform>();
+        foreach (var item in allgameObjects)
+        {
+            if (item == this.transform)
+                continue;
+            DestroyImmediate(item.gameObject); 
+        }
+        BoxColliders.Clear();
+    }
+}
--- Assets/Scripts/RandomPlacer.cs
+++ Assets/Scripts/RandomPlacer.cs
@@ -2,19 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//[ExecuteInEditMode]
+[RequireComponent(typeof(BoxCollider))]
 public class RandomPlacer : MonoBehaviour
 {
-
     public GameObject[] ObjectsToRandomlyPlace;
+    public int NumOfObjectsToPlace;
+    public float MinDistance;
+    public int MaxTries;
+    public Vector3 OffsetForRayCast;
+    BoxCollider _boxCollider;
+    List<GameObject> _placedObjects = new List<GameObject>();
 
-
-    //[ExecuteInEditMode]
     void Start()
     {
+        _boxCollider = GetComponent<BoxCollider>();
+        for (int i = 0; i < NumOfObjectsToPlace; i++)
+        {
+            GameObject possibleObjectToPlace = ObjectsToRandomlyPlace[Random.Range(0, ObjectsToRandomlyPlace.Length - 1)];
 
+            for (int j = 0; j < MaxTries; j++)
+            {
+                Vector3 possiblePosition = RandomPointInBounds(_boxCollider.bounds);
+                Quaternion rotation = Quaternion.Euler(0, Random.Range(0f, 360f), 0);
+
+                bool isTooCloseToOthers = false;
+
+                RaycastHit hitInfo;
+                //Ray ray = new Ray(possiblePosition , -Vector3.up);
+                Ray ray = new Ray(possiblePosition + OffsetForRayCast, -Vector3.up);
+
+                if (Physics.Raycast(ray, out hitInfo, 1000f) == false)
+                {
+                    //LineRenderer lineRenderer = new LineRenderer();
+                    //lineRenderer.SetPositions(new Vector3[](){ ray.origin, ray.or  } );
+                    Debug.Log("no hits");
+                    continue;
+                }
+
+                for (int o = 0; o < _placedObjects.Count; o++)
+                {
+
+                    // hit point is too close to others? if so we contine, else we set the possible position to the hit position
+                    if ((_placedObjects[o].transform.position - hitInfo.point).magnitude < MinDistance)
+                    {
+                        isTooCloseToOthers = true;
+                        Debug.Log("too close to others");
+                    }
+                    else
+                    {
+                        possiblePosition = hitInfo.point;
+                    }
+                }
+
+                if (isTooCloseToOthers == false)
+                {
+                    GameObject cloneObject = GameObject.Instantiate(possibleObjectToPlace, possiblePosition, rotation, this.transform);
+
+                    _placedObjects.Add(cloneObject);
+                    break;
+                }
+            }
+        }
     }
 
-    void Update()
+    public static Vector3 RandomPointInBounds(Bounds bounds)
     {
+        return new Vector3(
+            Random.Range(bounds.min.x, bounds.max.x),
+            Random.Range(bounds.min.y, bounds.max.y),
+            Random.Range(bounds.min.z, bounds.max.z)
+        );
     }
 }
